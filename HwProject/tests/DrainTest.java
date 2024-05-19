4bfbfd8ca9a2932bb7c265dbd92cd0c098d70971
Gergo Buzas <buzasgergo0615@gmail.com>
2024-05-19 04:43:36 +0200

    Added another game reset function, TODO: one function for instanceNr resets

--- HwProject/tests/DrainTest.java
+++ HwProject/tests/DrainTest.java
@@ -66,5 +66,6 @@ public class DrainTest {
         TestPump.setInstanceNr(0);
         TestPipe.setInstanceNr(0);
         TestSource.setInstanceNr(0);
+        TestDrain.setInstanceNr(0);
     }
 }
